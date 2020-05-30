b2ab5e94f3a53478b2e2672613a8f3404efc5caa
Peter <pc.dean@icloud.com>
2020-05-30 17:00:14 +0100

    write and solve test for 0+0

--- test/add_test.js
+++ test/add_test.js
@@ -6,5 +6,8 @@ describe('#add', function(){
     it('0+0=0', function(){
       expect(add('0','0')).to.equal('0');
     })
+    it('0+1=1', function(){
+      expect(add('0','1')).to.equal('1');
+    })
   })
 })
