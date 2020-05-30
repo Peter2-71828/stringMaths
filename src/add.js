91a0f14532c19661dff680066d9c6d7ef97c6f99
Peter <pc.dean@icloud.com>
2020-05-30 16:59:38 +0100

    write and solve test for 0+0

---
+++ src/add.js
@@ -0,0 +1,4 @@
+function add(num1, num2) {
+  return '0'
+}
+module.exports = add;
--- test/add_test.js
+++ test/add_test.js
@@ -1,2 +1,10 @@
 var expect = require('chai').expect
-require('../src/add.js');
+var add = require('../src/add.js');
+
+describe('#add', function(){
+  describe('#basic tests', function(){
+    it('0+0=0', function(){
+      expect(add('0','0')).to.equal('0');
+    })
+  })
+})
