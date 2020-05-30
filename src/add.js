53a415f66fb502fb8e114e92c5ad78c784768187
Peter <pc.dean@icloud.com>
2020-05-30 20:06:43 +0100

    rewrite function as a recursive function

--- src/add.js
+++ src/add.js
@@ -1,30 +1,50 @@
-function add(num1, num2) {
-  var ans = ""
-  var max;
-  var carry = 0;
-
+function add(num1, num2, carry=0){
+  var ans
   var length1 = num1.length
   var length2 = num2.length
-  if (length1 < length2){
-    max = length2
-  }else {
-    max = length1
-  }
-
-  for (i=1; i<=max; i++){
-    var value = Number(num1.charAt(length1-i)) + Number(num2.charAt(length2-i)) + carry
+  var value = Number(num1.charAt(length1-1))+Number(num2.charAt(length2-1))+carry
+  if (length1 > 1 || length2 > 1){
+    num1 = num1.slice(0, length1-1)
+    num2 = num2.slice(0, length2-1)
     if (value>9){
       value = (value%10).toString()
-      carry = 1
-    }else {
+      ans = add(num1, num2, 1).concat(value)
+    }else{
       value = value.toString()
-      carry = 0
+      ans = add(num1, num2, 0).concat(value)
     }
-    ans = value.concat(ans)
-  }
-  if (carry!=0){
-    ans = '1'.concat(ans)
-  }
-  return ans
+  }else{
+    ans = value.toString()
+  }return ans
 }
+
+// function add(num1, num2) {
+//   var ans = ""
+//   var max;
+//   var carry = 0;
+//
+//   var length1 = num1.length
+//   var length2 = num2.length
+//   if (length1 < length2){
+//     max = length2
+//   }else {
+//     max = length1
+//   }
+//
+//   for (i=1; i<=max; i++){
+//     var value = Number(num1.charAt(length1-i)) + Number(num2.charAt(length2-i)) + carry
+//     if (value>9){
+//       value = (value%10).toString()
+//       carry = 1
+//     }else {
+//       value = value.toString()
+//       carry = 0
+//     }
+//     ans = value.concat(ans)
+//   }
+//   if (carry!=0){
+//     ans = '1'.concat(ans)
+//   }
+//   return ans
+// }
 module.exports = add;
