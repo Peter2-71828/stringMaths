131bc188857023955bb91da189d7a931325aed77
Peter <pc.dean@icloud.com>
2020-06-08 16:54:31 +0100

    extract minus function into minus.js file

---
+++ src/minus.js
@@ -0,0 +1,20 @@
+function minus(num1, num2, carry=0){
+  var ans
+  var length1 = num1.length
+  var length2 = num2.length
+  var value = Number(num1.charAt(length1-1))-Number(num2.charAt(length2-1))-carry
+  if (length1 > 1 || length2 > 1){
+    num1 = num1.slice(0, length1-1)
+    num2 = num2.slice(0, length2-1)
+    if (value<0){
+      value = (value%0.1).toString()
+      ans = minus(num1, num2, 1).concat(value)
+    }else{
+      value = value.toString()
+      ans = minus(num1, num2, 0).concat(value)
+    }
+  }else{
+    ans = value.toString()
+  }return ans
+}
+module.exports = minus;
